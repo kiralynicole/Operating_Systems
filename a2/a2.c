99c37c4e19b1869944d10861a97a777d7ea3287f
kiralynicole <Kiraly.Pa.Nicole@student.utcluj.ro>
2023-04-30 21:17:15 +0300

    sync threads partial

--- a2/a2.c
+++ a2/a2.c
@@ -3,11 +3,37 @@
 #include<stdlib.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <pthread.h>
+#include <semaphore.h>
 #include "a2_helper.h"
+#define NR_THREADS 4
+
+
+typedef struct {
+int nb_prc;
+int nb_th;
+sem_t* logSem1;
+sem_t* logSem2;
+}TH_STRUCT;
+
+void* thread_func_sync(void* arg){
+    TH_STRUCT*threads = (TH_STRUCT *)arg;
+
+    info(BEGIN, threads->nb_prc, threads->nb_th);
+
+    info(END, threads->nb_prc, threads->nb_th);
+
+
+
+return NULL;
+}
 
 
 
 void process_hierarchy(){
+    pthread_t tid[NR_THREADS];
+    TH_STRUCT params[NR_THREADS];
+    sem_t logSem1, logSem2;
     pid_t pid2 = -1, pid3 = -1, pid4 = -1, pid5 = -1, pid6 = -1, pid7 = -1, pid8 = -1, pid9 = -1;
     int status2 = 0, status3 = 0, status4 = 0, status5 = 0, status6 = 0, status7 = 0, status8 = 0,status9 = 0;
 
@@ -96,11 +122,29 @@ void process_hierarchy(){
         }else{
 
             pid7 = fork();
+
             if(pid7 == -1){
                 perror("Could not create child process");
                  exit(-1);
             }else if(pid7 == 0){
             info(BEGIN, 7,0);
+
+            sem_init(&logSem1, 0, 0);
+            sem_init(&logSem2, 0, 0); 
+
+
+            for(int i=0; i < NR_THREADS; i++){
+                params[i].nb_prc = 7;
+                params[i].nb_th = i+1;
+                params[i].logSem1 = &logSem1;
+                params[i].logSem2 = &logSem2;
+                pthread_create(&tid[i], NULL, thread_func_sync, &params[i]);
+            }
+
+            for(int i = 0; i < NR_THREADS; i++){
+                pthread_join(tid[i], NULL);
+            }
+
             info(END, 7, 0);
             exit(7);
             }
