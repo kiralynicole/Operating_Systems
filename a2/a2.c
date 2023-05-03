6f6c0590df32e849a0b07870c538c139f169f6d4
kiralynicole <Kiraly.Pa.Nicole@student.utcluj.ro>
2023-05-03 16:42:22 +0300

    create threads in 3 and 4

--- a2/a2.c
+++ a2/a2.c
@@ -21,6 +21,19 @@ sem_t* logSem1;
 sem_t* logSem2;
 }TH_STRUCT;
 
+typedef struct {
+int nb_prc;
+int nb_th;
+sem_t* logSem1;
+}TH_STRUCT2;
+
+typedef struct {
+int nb_prc;
+int nb_th;
+sem_t* logSem1;
+}TH_STRUCT3;
+
+
 void* thread_func_sync(void* arg){
     TH_STRUCT*threads = (TH_STRUCT *)arg;
 
@@ -50,34 +63,48 @@ void* thread_func_sync(void* arg){
         sem_post(threads->logSem2);
     }
 
+return NULL;
+}
+
+void* thread_func_bar(void* arg){
+    TH_STRUCT2*threads = (TH_STRUCT2 *)arg;
+
+    sem_wait(threads->logSem1);
+
+    info(BEGIN, threads->nb_prc, threads->nb_th);
+
+    info(END, threads->nb_prc, threads->nb_th);
+
+    sem_post(threads->logSem1);
 
 
 return NULL;
 }
 
-// void* thread_func_bar(void* arg){
-//     TH_STRUCT*threads = (TH_STRUCT *)arg;
 
-// sem_wait(threads->logSem1);
+void* thread_func3(void* arg){
+    TH_STRUCT3*threads = (TH_STRUCT3 *)arg;
 
-//     info(BEGIN, threads->nb_prc, threads->nb_th);
+    info(BEGIN, threads->nb_prc, threads->nb_th);
 
-//     info(END, threads->nb_prc, threads->nb_th);
-
-// sem_post(threads->logSem1);
+    info(END, threads->nb_prc, threads->nb_th);
 
 
-// return NULL;
-// }
+return NULL;
+}
 
 
 
 void process_hierarchy(){
     pthread_t tid[NR_THREADS1];
     TH_STRUCT params[NR_THREADS1];
-    // pthread_t tid2[NR_THREADS2];
-    //TH_STRUCT params2[NR_THREADS2];
+     pthread_t tid2[NR_THREADS2];
+    TH_STRUCT2 params2[NR_THREADS2];
+    pthread_t tid3[NR_THREADS3];
+    TH_STRUCT3 params3[NR_THREADS3];
     sem_t logSem1, logSem2;
+    sem_t logSem_5;
+    sem_t logSem_8;
     pid_t pid2 = -1, pid3 = -1, pid4 = -1, pid5 = -1, pid6 = -1, pid7 = -1, pid8 = -1, pid9 = -1;
     int status2 = 0, status3 = 0, status4 = 0, status5 = 0, status6 = 0, status7 = 0, status8 = 0,status9 = 0;
 
@@ -115,22 +142,24 @@ void process_hierarchy(){
         perror("Could not create child process");
         exit(-1);
         }else if (pid5 == 0){
+
         info(BEGIN, 5, 0);
-        // sem_t logSem_5;
-        // sem_init(&logSem_5, 0, 4);
 
-        //  for(int i=0; i < NR_THREADS2; i++){
-        //         params[i].nb_prc = 5;
-        //         params[i].nb_th = i+1;
-        //         params[i].logSem1 = &logSem_5;
-        //         pthread_create(&tid2[i], NULL, thread_func_bar, &params2[i]);
-        //     }
+        sem_init(&logSem_5, 0, 5);
 
-        //     for(int i = 0; i < NR_THREADS2; i++){
-        //         pthread_join(tid2[i], NULL);
-        //     }
 
-        //     sem_destroy(&logSem_5);
+         for(int i=0; i < NR_THREADS2; i++){
+                params2[i].nb_prc = 5;
+                params2[i].nb_th = i+1;
+                params2[i].logSem1 = &logSem_5;
+                pthread_create(&tid2[i], NULL, thread_func_bar, &params2[i]);
+            }
+
+            for(int i = 0; i < NR_THREADS2; i++){
+                pthread_join(tid2[i], NULL);
+            }
+
+            sem_destroy(&logSem_5);
 
 
         info(END, 5,0);
@@ -171,6 +200,21 @@ void process_hierarchy(){
                 }else{
                     waitpid(pid9, &status9, 0);
                 }
+                sem_init(&logSem_8,0,0);
+
+            for(int i=0; i < NR_THREADS3; i++){
+                params3[i].nb_prc = 8;
+                params3[i].nb_th = i+1;
+                params3[i].logSem1 = &logSem_8;
+                pthread_create(&tid3[i], NULL, thread_func3, &params3[i]);
+            }
+
+            for(int i = 0; i < NR_THREADS3; i++){
+                pthread_join(tid3[i], NULL);
+            }
+
+            sem_destroy(&logSem_8);
+                
 
                 info(END, 8, 0);
                 exit(8);
@@ -229,15 +273,15 @@ void process_hierarchy(){
 int main(){
     init();
 
-    sem1 = sem_open("semaphore1", O_CREAT, 0644, 1);
-if(sem1 == NULL) {
-     perror("Could not aquire the semaphore");
- }
+//     sem1 = sem_open("semaphore1", O_CREAT, 0644, 1);
+// if(sem1 == NULL) {
+//      perror("Could not aquire the semaphore");
+//  }
 
-sem2 = sem_open("semaphore2", O_CREAT, 0644, 1);
-if(sem2 == NULL) {
-     perror("Could not aquire the semaphore");
- }
+// sem2 = sem_open("semaphore2", O_CREAT, 0644, 1);
+// if(sem2 == NULL) {
+//      perror("Could not aquire the semaphore");
+//  }
     info(BEGIN, 1, 0);
 
     process_hierarchy();
