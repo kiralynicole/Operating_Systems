95d862cdf27b295a7f7e8ad3f8609a43718c9767
kiralynicole <Kiraly.Pa.Nicole@student.utcluj.ro>
2023-05-28 20:22:48 +0300

    sec push

--- a3/a3.c
+++ a3/a3.c
@@ -5,6 +5,7 @@
 #include <unistd.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
+#include <stdbool.h>
 #include <sys/mman.h>
 #include <string.h>
 #define FIFO1 "RESP_PIPE_64529"
@@ -14,6 +15,12 @@
 int main(int argc, char** argv){
 
     char connect[8] = {0x07, 0x43, 0x4f, 0x4e, 0x4e, 0x45, 0x43, 0x54};
+    char request[4] = {0x50, 0x49, 0x4E, 0x47};
+    char request2[4] = {0x50, 0x4f, 0x4e, 0x47};
+    unsigned int variant = 64529;
+   // char success[] = {0x07, 0x53, 0x55, 0x43, 0x43,  0x45, 0x53,  0x53};
+    //char err[] = {0x05, 0x45,  0x52,  0x52, 0x4f, 0x52};
+
 
     if(mkfifo(FIFO1, 0600) != 0){
         printf("EROR\ncannot create the response pipe");
@@ -38,6 +45,38 @@ int main(int argc, char** argv){
             printf("SUCCESS\n");
         }
 
+        
+
+        //while(true){
+        unsigned char received_size;
+
+        if(read(fd2, &received_size, sizeof(unsigned char)) == 1){
+         
+            char received[4];
+            
+            read(fd2, received, received_size);
+           // received[received_size] = '\0';
+             
+            //  if(strcmp(received, "EXIT") == 0){
+            //     break;
+            // }
+           
+
+            if(strncmp(received, "PING",4) == 0){
+                write(fd1, &received_size, 1);
+                write(fd1, request, sizeof(request));
+                write(fd1, &variant, sizeof(unsigned int));
+                write(fd1, &received_size, sizeof(unsigned char));
+                write(fd1, request2, sizeof(request2));
+            }
+
+
+           
+        }
+     //  }
+
+        
+        
 
 close(fd1);
 close(fd2);
@@ -46,5 +85,8 @@ unlink(FIFO1);
 return 0;
 
     }
+
+
+    
 
 
